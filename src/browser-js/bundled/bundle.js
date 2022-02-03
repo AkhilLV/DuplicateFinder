d6574f7d25f81da69d454abf01f05fb92e3b53ad
Akhil <akhillav@gmail.com>
2022-02-03 22:09:13 +0530

    add directory bug fixes

--- src/browser-js/bundled/bundle.js
+++ src/browser-js/bundled/bundle.js
@@ -23,14 +23,24 @@ class Directories {
     if (!this.isParentIncluded(directoryPath)) {
       this.directories.add(directoryPath);
     }
+
+    const [isChildIncluded, childDirectoryPaths] = this.isChildIncluded(directoryPath);
+
+    if (isChildIncluded) {
+      childDirectoryPaths.forEach((childDirectoryPath) => {
+        this.directories.delete(childDirectoryPath);
+        this.directories.add(directoryPath);
+      });
+    }
   };
 
   isParentIncluded = (directoryPath) => {
+    // if home/pictures is included, home/pictures/new-york is not allowed
+
     let isParentIncluded = false;
 
-    this.directories.forEach((directory) => {
-      const regex = new RegExp(directory);
-      if (directoryPath.match(regex)) {
+    this.directories.forEach((existingDirectory) => {
+      if (directoryPath.match(existingDirectory)) {
         isParentIncluded = true;
       }
     });
@@ -38,13 +48,22 @@ class Directories {
     return isParentIncluded;
   };
 
-  // isChildIncluded = (directoryPath) => {
-  //   let isChildIncluded = false;
+  isChildIncluded = (directoryPath) => {
+    // eslint-disable-next-line max-len
+    // if home/pictures/new-york is included, home/pictures is allowed after removing home/pictures/new-york, home/pictures/boston
 
-  //   this.directories.forEach((directory) => {
-  //     if ()
-  //   });
-  // }
+    let isChildIncluded = false;
+    const childDirectoryPaths = [];
+
+    this.directories.forEach((existingDirectory) => {
+      if (existingDirectory.match(directoryPath) && existingDirectory !== directoryPath) {
+        isChildIncluded = true;
+        childDirectoryPaths.push(existingDirectory);
+      }
+    });
+
+    return [isChildIncluded, childDirectoryPaths];
+  };
 }
 
 const directories = new Directories();
